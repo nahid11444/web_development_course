c8ea79565b6fafce65dcb20be8aa629dc4cfe3e0
KHANDAKER MEHEDI HASAN <khandakernahidmil3@gmail.com>
2022-06-17 12:04:19 +0600

    jun 17 sixth

---
+++ logout.php
@@ -0,0 +1,16 @@
+<?php
+    session_start();
+
+    echo 'Before logout';
+
+    echo '<pre>';
+    print_r($_SESSION);
+    echo '</pre>';
+
+    session_unset();
+
+    echo '<pre>';
+    print_r($_SESSION);
+    echo '</pre>';
+
+?>
--- user_details.php
+++ user_details.php
@@ -5,12 +5,15 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>User_details_session</title>
-    <?php session_start() ?>
+    <?php session_start(); ?>
 </head>
 <body>
     Logged in user details:
     <br>
     Username : <?php echo $_SESSION['username'].'<br>'; ?>
     Password : <?php echo $_SESSION['password'];?>
+    <br>
+
+    <a href="logout.php"> <button type="button">Logout</button> </a>
 </body>
 </html>
